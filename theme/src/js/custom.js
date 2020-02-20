bef32bd1915933a2a2385e82ee1492c622fb5f65
Jan Heder <info@janheder.cz>
2020-02-21 00:29:45 +0100

    css progress

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -1,2 +1,15 @@
 
 $(".search").before('<div class="header-contacts"><span>+420 739 787 164</span><span>info@benlemi.cz</span></div>');
+
+/*
+var fragment = document.createDocumentFragment();
+
+var e = document.createElement("div");
+e.className = "header-contacts";
+e.innerHTML = '<span>+420 739 787 164</span><span>info@benlemi.cz</span>';
+fragment.appendChild(e);
+
+var sp2 = document.getElementsByClassName("search")[0];
+var parentDiv = sp2.parentNode;
+parentDiv.insertBefore(fragment, sp2);
+*/
