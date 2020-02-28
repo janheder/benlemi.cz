08f21c2c5314393da1f531de4cd60a9910d48287
Jan Heder <info@janheder.cz>
2020-02-28 16:39:39 +0100

    css progress

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -1,8 +1,9 @@
 
 // -----------------------------------------------------------------------------
-// ADD HEADER CONTACT INFO
+// HEADER AREA
 // -----------------------------------------------------------------------------
 
+/* add contacts into header area */ 
 if ($(".search").length){
     var mail = $("#footer .mail").text();
     var tel = $("#footer .tel").text();
@@ -13,14 +14,16 @@ if ($(".search").length){
     '</div>');
 }
 
+/* add search and user icons to header area */ 
 $(".navigation-buttons").prepend('<div class="nav-search" id="js-searchToggle"></div><a href="/login" class="nav-user"></a>');
 
+/* add hamburger menu on mobile */ 
 $(".header-top").prepend('<div class="nav-menu-toggle" id="js-menuToggle"><span></span></div>');
 
+/* toggle control of responsive menu */ 
 $("#js-menuToggle").click(function(){
     $(".header-top").toggleClass("--active");
     $("body").toggleClass("--noScroll");
-    /*$("body").toggleClass("--active");*/
 });
 
 
@@ -38,7 +41,7 @@ if ($("#blogSection").length){
     $("#blogSection .container").load("/blog/ .news-wrapper");
 }
 
-/* relocate points depending on page type */
+/* relocate benefit points depending on page type */
 if ($("#pointsSection").length){
     if ($("body.type-index").length){
         $("#pointsSection").insertAfter(".before-carousel");
