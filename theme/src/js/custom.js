ceeefaa945bb04ce8c4c084870d3b6b70a558aae
Jan Heder <info@janheder.cz>
2020-03-19 22:11:02 +0100

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -175,3 +175,10 @@ if ($(".type-category").length){
 // CART STEP 1
 // -----------------------------------------------------------------------------
 
+if ($(".cart-table").length){
+    $("<div class='cart-table-heading'></div>").insertBefore(".cart-table tbody");
+    $(".removeable:first-child .p-label").each(function(){
+        var label = $(this).html();
+        $(".cart-table-heading").prepend("<span>" + label + "</span>");
+    });
+}
