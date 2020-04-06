3c423644e1fb5ba8900ce4717b2980e9985d723d
Jan Heder <info@janheder.cz>
2020-04-06 14:45:57 +0200

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -332,5 +332,5 @@ if ($("#rate-form").length){
 /* load blog posts into homepage section */
 if ($(".blogCategories").length){
     $(".blogCategories .blogCategories__domacnost").load("/blog-domacnost/ .news-wrapper");
-    $(".blogCategories .blogCategories__domacnost").prepend("<h2>Domácnost</h2>");
+    $("<h2 class='blogCategories__sectionTitle'>Domácnost</h2>").inserBefore(".blogCategories .blogCategories__domacnost");
 }
