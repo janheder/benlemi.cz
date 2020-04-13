b5d35c786b93acddcf302b9d61a009fdb7c82fa1
Jan Heder <info@janheder.cz>
2020-04-13 19:17:55 +0200

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -292,40 +292,36 @@ if ($(".type-detail").length){
         var str = $(this).text();
         var strM = str.replace(/\s/g, '',).replace("Kč", '').replace("od", ''); 
         $(this).text(strM);
-        var str = $(this).append("<span> Kč</span>");
+        var str = $(this).append("<span class='price-final-currency'> Kč</span>");
     });
 
-    $(".price-final span.default-variant").prepend("<span>od </span>");
+    $(".price-final span.default-variant").prepend("<span class='price-final-pre'>od </span>");
 
     $(".price-additional span").each(function() {
         var str1 = $(this).text();
         var strM1 = str1.replace(/\s/g, '',).replace("KčbezDPH", '').replace("od", ''); 
         $(this).text(strM1);
-        var str1 = $(this).append("<span> Kč bez DPH</span>"); 
+        var str1 = $(this).append("<span class='price-final-dph'> Kč bez DPH</span>"); 
     });
 
-    $(".price-additional span.default-variant").prepend("<span>od </span>");
+    $(".price-additional span.default-variant").prepend("<span class='price-final-pre'>od </span>");
 
     $(".price-standard span").each(function() {
         var str1 = $(this).text();
         var strM1 = str1.replace(/\s/g, '',).replace("Kč", '').replace("od", ''); 
         $(this).text(strM1);
-        var str1 = $(this).append("<span> Kč</span>");
+        var str1 = $(this).append("<span class='price-final-currency'> Kč</span>");
     });
 
-    $(".price-standard span.default-variant").prepend("<span>od </span>");
+    $(".price-standard span.default-variant").prepend("<span class='price-final-pre'>od </span>");
 
     $(".price-additional:not(:has(span))").each(function() {
         var str1 = $(this).text();
         var strM1 = str1.replace(/\s/g, '',).replace("KčbezDPH", ''); 
         $(this).text(strM1);
-        var str1 = $(this).append("<span> Kč bez DPH</span>");
+        var str1 = $(this).append("<span class='price-final-dph'> Kč bez DPH</span>");
     });
 }
-
-
-
-
 
 
 // -----------------------------------------------------------------------------
