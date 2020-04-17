c9fe58b8ed58bda94dec3a7fed68eb06ac807f87
Jan Heder <info@janheder.cz>
2020-04-17 16:03:36 +0200

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -397,8 +397,10 @@ if ($("#rate-form").length){
 if ($(".blogCategories").length){
     $(".blogCategories .blogCategories__domacnost").load("/blog-domacnost/ .news-wrapper");
     $("<h2 class='blogCategories__sectionTitle'>Domácnost</h2>").insertBefore(".blogCategories .blogCategories__domacnost");
-    $("<div class='blogCategories__sectionShowMore'><a href='/blog-domacnost/' class='blogCategories__sectionShowMoreLink'>Více článků</a></div>").insertAfter(".blogCategories__section .news-item:nth-child(2)");
-    
+    $( document ).ready(function() {
+        $("<div class='blogCategories__sectionShowMore'><a href='/blog-domacnost/' class='blogCategories__sectionShowMoreLink'>Více článků</a></div>").insertAfter(".blogCategories__section .news-item:nth-child(2)");
+    });
+
     $(".blogCategories .blogCategories__o-nas").load("/blog-o-nas/ .news-wrapper");
     $("<h2 class='blogCategories__sectionTitle'>O nás</h2>").insertBefore(".blogCategories .blogCategories__o-nas");
 }
