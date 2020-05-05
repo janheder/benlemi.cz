5680eb519c1dcd089e4fa26792ae19851ed4adb4
Jan Heder <info@janheder.cz>
2020-05-05 09:37:40 +0200

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -593,7 +593,7 @@ if ($(".ordering-process").length){
 if ($(".in-krok-2").length){
     $(".in-krok-2 .co-billing-address .unveil-wrapper").insertAfter('.in-krok-2 .co-billing-address fieldset > h4');
 
-    $(".stay-in-touch .form-group:last-child").addClass("register");
+    $(".stay-in-touch .form-group:nth-child(2)").addClass("register");
     $(".stay-in-touch .form-group.register").insertAfter(".co-contact-information");
 }
 
