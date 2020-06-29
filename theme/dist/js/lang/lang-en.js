104d7dbaa0de096cc2af673117299698bb2290eb
Jan Heder <info@janheder.cz>
2020-06-29 15:22:41 +0200

    Update lang-en.js

--- theme/dist/js/lang/lang-en.js
+++ theme/dist/js/lang/lang-en.js
@@ -125,4 +125,4 @@ var g_inClientDiscussionUrl = "client-discussion";
 var g_inForgotPasswordUrl = "forgotten-password";
 
 
-var relatedCats = ".in-children-s-house-beds, .in-children-s-single-beds";
+var relatedCats = ".in-children-s-house-beds, .in-children-s-single-beds, .in-children-s-bunk-and-loft-beds, .in-bed-accesory, .in-under-bed-drawers, .in-children-s-mattresses, .in-children-s-wardrobes, .in-children-s-tables-and-chairs, .in-single-beds, .in-double-beds, .in-bed-accesory-2, .in-sheets, .in-mattress-protectors";
