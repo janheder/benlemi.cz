474163f6384bf0cfeb5e953065d893bab79b3346
Jan Heder <info@janheder.cz>
2020-07-01 23:49:36 +0200

    css

--- theme/src/js/custom.js
+++ theme/src/js/custom.js
@@ -509,8 +509,8 @@ $(document).ready(function() {
 
 
 /* Adjust price displaying */
-/*
-if ($(".type-detail").length){
+
+if ($(":lang(cs) .type-detail").length){
 
     $(".price-final span").each(function() {
         var str = $(this).text();
@@ -546,7 +546,7 @@ if ($(".type-detail").length){
         var str1 = $(this).append("<span class='price-final-dph'> " + g_excludingVat + "</span>");
     });
 }
-*/
+
 /* rename titles */
 if ($(".type-detail").length){
     $(".extended-description h3").text(g_advantagesAndSpecifications);
