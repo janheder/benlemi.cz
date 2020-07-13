a1954171d2253d282296a8fae2efa0b4074af237
Jan Heder <info@janheder.cz>
2020-07-13 13:14:46 +0200

    css

--- theme/src/js/custom.index.js
+++ theme/src/js/custom.index.js
@@ -10,7 +10,7 @@ if ($("#homepageSection").length){
 
 /* load blog posts into homepage section */
 if ($("#blogSection").length){
-    $("#blogSection .blogSection__content").load("/" + category1Url + "/ .news-wrapper .news-item:nth-child(n+3)");
+    $("#blogSection .blogSection__content .news-wrapper").load("/" + category1Url + "/ .news-wrapper .news-item:nth-child(-n+3)");
 }
 
 /* load rating into homepage section */
