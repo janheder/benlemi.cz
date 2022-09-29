ace40878259d8df44a839a5608d5232fc7a5fc4a
Jan Heder <info@janheder.cz>
2022-09-29 19:05:44 +0200

    Create custom.category.js

---
+++ theme/dist/js/new/custom.category.js
@@ -0,0 +1,122 @@
+// -----------------------------------------------------------------------------
+// PRODUCT CATEGORY
+// -----------------------------------------------------------------------------
+
+/* relocate heading and categories */
+if ($(".type-category").length){
+    $(".category-title").insertBefore(".content-wrapper-in");
+    $(".subcategories").insertAfter(".category-title");
+    $(".category-perex").insertAfter(".category-title");
+}
+
+/* relocate sort into filters */
+if ($(".filters-wrapper").length){
+    $("#content-wrapper").append('<div id="filtersToggle">' + g_openFilter + '</div>');
+    $("#filtersToggle").click(function(){
+        $("#content-wrapper").toggleClass("--showFilters");
+        if ($("#content-wrapper").hasClass("--showFilters")) {
+            $("#filtersToggle").text(g_hideFilter);
+        }else{
+            $("#filtersToggle").text(g_showFilter);
+        }
+    });
+}
+
+/* rename show more button */;
+if ($(".pagination-loader").length){
+    $(".pagination-loader span").text(g_showMore);
+}
+
+
+function productCardEdit(){ 
+
+    /* relocate hover overlay on product & add more variants into product box */
+    if ($(".product").length){
+        $(".product").each(function(){
+            $(this).find(".p-tools .btn").text(g_show);
+
+            if ($(this).find(".price-final strong > small").length){
+                $(this).find(".name").append("<span class='p-variants'>" + g_moreVariants + "</span>");
+            }
+        });
+    }
+
+    /* relocate flags on mobile */
+
+    if ($(".product").length){
+        $(".product").each(function(){
+            $(this).find(".flags-default").insertBefore($(this).find(".p-in-in"));
+        });
+    }
+
+    $(".p").each(function(){
+        var price = $(this).find(".flags .flag .price-standard").html();
+        $(this).find(".prices span").remove();
+        $(this).find(".prices").prepend(price);
+    });
+
+    if ($(".product .flag-discount:contains('až')").length){
+        $(".p").each(function() {
+            var a = $(this).find(".flag-discount:contains('až')").html();
+            if(a === undefined){
+                
+            }else{
+                var b = a.replace('až','');
+                $(this).find(".flag-discount:contains('až')").html(b);
+            }
+        });
+    }
+}
+
+productCardEdit();
+
+document.addEventListener('ShoptetDOMPageContentLoaded', function () {
+    productCardEdit();
+});
+document.addEventListener('ShoptetDOMPageMoreProductsLoaded', function () {
+    productCardEdit();
+});
+
+
+
+/* category header edit */
+if ($("#category-header").length){
+
+
+    $("#filterToggleDesktop").click(function(){
+        $(".sidebar-left").toggleClass("--active");
+
+        if($("#filterToggleDesktop").hasClass("active")){
+            $(this).text(g_showFilter);
+            $(this).removeClass("active");
+        }
+        else{
+            $(this).text(g_hideFilter);
+            $(this).addClass("active");
+        }
+    });
+}
+
+/* category perex */
+if ($(".category-perex").length){
+    if ($(window).width() <= 991) {
+        var text = $(".category-perex p").text();
+        var textMod = text.replace(/(([^\s]+\s\s*){40})(.*)/,"$1…");
+        $("<div class='perex-short'></div><div class='perex-showMore'>" + g_fullDescription + "</div>").insertAfter(".category-perex > p");
+        $(".perex-short").text(textMod);
+        $(".perex-showMore").click(function(){
+            $(".category-perex").addClass("active");
+        });
+    }
+}
+
+/* category header title */
+if ($(".category-header").length){
+    var catHead = $(".category-header div strong").html();
+    $(".category-header div:last-child").text(g_totalQuantity + ":" + catHead);
+}
+
+/*
+$(".filters-wrapper").append("<div class='filter-contact'></div>");
+$(".filter-contact").load(g_cart1Url + " .checkout-box");
+*/
